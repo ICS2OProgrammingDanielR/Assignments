13a8a3a7ad4b6261187a8739c7d9dce0ba80726b
Daniel Raissi <55556023+s277036@users.noreply.github.com>
2019-12-19 16:10:08 -0500

    gfdfgh

---
+++ PrankSoccerDaniel/you_lose.lua
@@ -0,0 +1,226 @@
+----------------------------------------------------------------------------------------
+--
+--  you_lose.lua
+-- Created by: Noah
+-- Date: December 11th, 2019
+-- Description: This is the lose screen, displaying the lose text & back button.
+-----------------------------------------------------------------------------------------
+display.setStatusBar(display.HiddenStatusBar)
+-----------------------------------------------------------------------------------------
+-- INITIALIZATIONS
+-----------------------------------------------------------------------------------------
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Use Widget Library
+local widget = require( "widget" )
+
+-----------------------------------------------------------------------------------------
+
+-- Naming Scene
+sceneName = "you_lose"
+
+-----------------------------------------------------------------------------------------
+
+-- Creating Scene Object
+local scene = composer.newScene( sceneName )
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+local background
+local backButton
+local sceneCover
+local sceneCover2
+
+
+-----------------------------------------------------------------------------------------
+-- LOCAL SOUNDS
+-----------------------------------------------------------------------------------------
+
+local transitionSound = audio.loadStream("Sounds/bop.mp3")
+local transitionSoundChannel
+local loseSound = audio.loadStream("Sounds/gameOver.mp3")
+local loseSoundChannel
+
+-----------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+
+-- Creating Transition to Main Menu Screen
+local function MainMenuTransition( )
+    composer.gotoScene( "main_menu", {effect = "slideDown", time = 1000})
+    
+
+    transitionChannel = audio.play(transitionSound)
+end    
+
+-- fades in the back button
+local function ButtonFade( event )
+    backButton.alpha = backButton.alpha + 0.006
+end
+
+local function CoverMove( event )
+    sceneCover.x = sceneCover.x - 2
+    sceneCover2.x = sceneCover2.x + 2
+end
+-- spins the tittle
+
+----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+    -- BACKGROUND IMAGE & STATIC OBJECTS
+    -----------------------------------------------------------------------------------------
+  -- creating the background
+   background = display.newImageRect("Images/YouWinScreenNoah@2x.png", display.contentWidth, display.contentHeight)
+   background.x = display.contentCenterX
+   background.y = display.contentCenterY
+
+    sceneGroup:insert( background )
+
+   sceneCover = display.newRect(display.contentWidth - 1024, display.contentHeight/2, display.contentWidth, display.contentHeight)
+   sceneCover2 = display.newRect(display.contentWidth, display.contentHeight/2, display.contentWidth, display.contentHeight)
+
+    sceneGroup:insert( sceneCover )
+    sceneGroup:insert( sceneCover2 )
+
+
+
+    -----------------------------------------------------------------------------------------
+    -- BUTTON WIDGETS
+    -----------------------------------------------------------------------------------------   
+
+    -- Creating Play Button
+    backButton = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentWidth/2,
+            y = display.contentHeight - 100,
+            width = 400,
+            height = 200,
+            
+
+            -- Insert the images here
+            defaultFile = "Images/BackButtonUnpressedNoah@2x.png",
+            overFile = "Images/BackButtonPressedNoah@2x.png",
+
+            -- When the button is released, call the main menu screen transition function
+            onRelease = MainMenuTransition          
+        } )
+       
+        backButton.alpha = 0
+
+       
+    -----------------------------------------------------------------------------------------
+    
+    -----------------------------------------------------------------------------------------
+    sceneGroup:insert( backButton )
+
+end  
+
+
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+   Runtime:addEventListener("enterFrame", CoverMove)
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).   
+    if ( phase == "will" ) then
+       
+    -----------------------------------------------------------------------------------------
+ 
+    -- Called when the scene is now on screen.
+    -- Insert code here to make the scene come alive.
+    -- Example: start timers, begin animation, play audio, etc.
+    elseif ( phase == "did" ) then 
+       
+        if (soundOn == true) then
+
+            loseSoundChannel = audio.play(loseSound)
+  
+                       
+        end
+       
+
+    end
+
+end -- function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    if ( phase == "will" ) then
+        -- Called when the scene is on screen (but is about to go off screen).
+        -- Insert code here to "pause" the scene.
+        -- Example: stop timers, stop animation, stop audio, etc.
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- Called immediately after scene goes off screen.
+       
+       
+    end
+
+end -- function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+
+
+return scene
